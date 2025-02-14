c540147e96a7d4c62b748d6c6d8b92a0fac5c167
CokeBot <longyuzhou1998@gmail.com>
2025-02-14 09:55:33 +0800

    step2 修复找不到WindowsUpdate文件的问题

--- step2_gpedit.py
+++ step2_gpedit.py
@@ -1,6 +1,5 @@
 import winreg
 
-
 '''
 修改注册表的值实现修改组策略的效果
 要注意：组策略是记录历史状态的，不会读取注册表的值同步，所以组策略是看不到修改后的状态的，
@@ -10,21 +9,17 @@ import winreg
 
 def disable_automatic_updates() -> None:
     # 定义注册表路径
-    reg_path = r"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
+    reg_path = r"SOFTWARE\Policies\Microsoft\Windows"
     # 打开或创建 WindowsUpdate 路径
     with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, reg_path, 0, winreg.KEY_ALL_ACCESS) as key:
         try:
-
+            # 尝试打开 WindowsUpdate 子路径，如果不存在则创建
+            wu_key = create_key_if_not_exist(key, "WindowsUpdate")
             # 设置 SetDisableUXWUAccess 为1，删除扫描 Windows 更新所需的访问权限
-            winreg.SetValueEx(key, "SetDisableUXWUAccess", 0, winreg.REG_DWORD, 1)
+            winreg.SetValueEx(wu_key, "SetDisableUXWUAccess", 0, winreg.REG_DWORD, 1)
 
             # 尝试打开 AU 子路径，如果不存在则创建
-            try:
-                au_key = winreg.OpenKey(key, "AU", 0, winreg.KEY_ALL_ACCESS)
-            except FileNotFoundError:
-                # 如果 AU 子项不存在，创建它
-                au_key = winreg.CreateKey(key, "AU")
-
+            au_key = create_key_if_not_exist(wu_key, "AU")
             # 设置 NoAutoUpdate 为 1，禁用自动更新
             winreg.SetValueEx(au_key, "NoAutoUpdate", 0, winreg.REG_DWORD, 1)
 
@@ -35,6 +30,21 @@ def disable_automatic_updates() -> None:
         finally:
             if au_key:
                 winreg.CloseKey(au_key)
+            if wu_key:
+                winreg.CloseKey(wu_key)
+
+
+def create_key_if_not_exist(
+        key: winreg.HKEYType,
+        sub_key_name: str
+) -> winreg.HKEYType:
+    # 尝试打开 sub_key 子路径，如果不存在则创建
+    try:
+        sub_key = winreg.OpenKey(key, sub_key_name, 0, winreg.KEY_ALL_ACCESS)
+    except FileNotFoundError:
+        # 如果 sub_key 子项不存在，创建它
+        sub_key = winreg.CreateKey(key, sub_key_name)
+    return sub_key
 
 
 def main():
